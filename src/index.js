6c2ca74d9f76b1f4c75f67934c0c1a949c7d6629
jrdc854 <1234jordic@gmail.com>
2025-11-26 15:27:22 +0100

    Setup Travis CI + Semantic Release

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 var cervezas = require('./cervezas.json');
 var uniqueRandomArray = require('unique-random-array').default || require('unique-random-array');
+var _ = require('lodash');
 
 module.exports = {
-  todas: cervezas,
-  alazar: uniqueRandomArray(cervezas)
+  todas: _.sortBy(cervezas, ['nombre']),
+  alazar: uniqueRandomArray(cervezas) // ahora sí es función
 };
